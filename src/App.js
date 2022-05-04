a5749d2d9501f4e4ae2f083efe17ea9d319f8737
Aravind <sekararavind5@gmail.com>
2022-05-04 19:46:19 +0530

    rlogo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import pic from './rocket-ship.png'
 import Info from './components/Info'
 import About from './components/About'
 import Footer from './components/Footer'
 function App() {
   return (
-    <div className="App">
-      <Info/>
-      <About/>
-      <Footer/>
+    <div >
+      <div className="App">
+        <Info/>
+        
+        <About/>
+        <Footer/>
+        
+      </div>
+      <div className='apic'>
+        <img  src={pic} alt="rpic" />
+      </div>
     </div>
   );
 }
